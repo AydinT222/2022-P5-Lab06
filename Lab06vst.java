abc91a290d0d5972cd958b9b12435b5e111dd2b7
AydinT222 <70951773+AydinT222@users.noreply.github.com>
2022-11-14 10:00:30 -0500

    unfinished, should finish by tomorrow

--- Lab06vst.java
+++ Lab06vst.java
@@ -8,7 +8,16 @@ import java.util.*;
 
 public class Lab06vst extends Applet
 {
-
+    public int minMaxRand(int min, int max) {
+        return (int)(Math.floor(Math.random() * (max - min)) + min); // it kept complaining about not accepting doubles, so had to cast to int
+    }
+    public void randomColor(Graphics g) { // used to do public int[] but forgot how to pass them as args instead of as an array
+        g.setColor(new Color(
+            (int)(Math.random() * 255),
+            (int)(Math.random() * 255),
+            (int)(Math.random() * 255)
+        )); // same with this
+    }
     public void paint(Graphics g)
     {
         // Draw Grid
@@ -19,25 +28,45 @@ public class Lab06vst extends Applet
 
 
         // Draw Random Lines
-
-
-
-
+        for (int i = 1; i <= 100; i++) {
+            g.drawLine(
+                minMaxRand(10, 390),
+                minMaxRand(10, 300),
+                minMaxRand(10, 390),
+                minMaxRand(10, 300)
+            );
+            randomColor(g); 
+        }
 
         // Draw Random Squares
-
-
-
+        for (int i = 1; i <= 100; i++) {
+            g.fillRect(
+                minMaxRand(410, 740),
+                minMaxRand(10, 250),
+                50,
+                50
+            );
+            randomColor(g); 
+        }
 
 
         // Draw Random Circles
-
+        for (int i = 1; i <= 100; i++) {
+            int size = minMaxRand(0, 200);
+            g.drawOval(
+                minMaxRand(10, 400 - size),
+                minMaxRand(300, 590 - size),
+                size,
+                size
+            );
+            randomColor(g); 
+        }
 
 
 
 
         // Draw 3-D Box
-
+        // polygons?
 
 
 
